9a89bad019b32d402368dcd53c61e1a99d424c30
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2021-09-10 21:59:47 -0400

    Update cliente_blockchain.php

--- cliente_blockchain.php
+++ cliente_blockchain.php
@@ -2,13 +2,16 @@
 use GuzzleHttp\Client as HttpClient;
 
 class cBlockChain
-{
-
+{   
+    protected $apiVer= 1;
+    protected $url= 'https://blockchain.detecton.us/api';
+    
     public function create_invoice($data)
     {
         try
         {
-        $url= 'https://blockchain.detecton.us/api/v1/create';
+        $endPoint= 'create'; /* Endpoint del API */
+        $url= $this->url . '/v' . $this->apiVer . '/' . $endPoint;
         $client = new HttpClient();
         $res = $client->request('GET', $url, ['query' => $data]);
           return $res->getStatusCode();
