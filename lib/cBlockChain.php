bcb70dfc117b66be825a61689ca77048c22a73e7
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2021-09-13 09:32:50 -0400

    Update cBlockChain.php

--- lib/cBlockChain.php
+++ lib/cBlockChain.php
@@ -7,16 +7,23 @@ class cBlockChain
 {
     protected $apiVer= 1;
     protected $url= 'https://blockchain.detecton.us/api';
-    
+    private $token= '';
+
+    private function __construct($token)
+    {
+        $this->token= $token;
+    }
+
     public function create_invoice($data)
     {
         try
         {
         $endPoint= 'create'; /* Endpoint del API */
         $url= $this->url . '/v' . $this->apiVer . '/' . $endPoint;
+        $data['token']= $this->token;
         $client = new HttpClient();
         $res = $client->request('GET', $url, ['query' => $data]);
-          return $res->getStatusCode();
+            return $res->getStatusCode();
         } catch(\Exception $ex) {
             //echo $ex;
         }
